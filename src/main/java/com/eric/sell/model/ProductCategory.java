fc82e4f3d36b15feb9f2b99bfacbb24c3866abfc
elevencityhost <15901506742@163.com>
2018-05-16 22:01:53 +0800

    使用lombok插件所带的注解@Data，能够自动生成get、set、toString方法

--- src/main/java/com/eric/sell/model/ProductCategory.java
+++ src/main/java/com/eric/sell/model/ProductCategory.java
@@ -1,5 +1,7 @@
 package com.eric.sell.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,43 +13,14 @@ import javax.persistence.Id;
  * @data 2018/5/14 23:58
  */
 @Entity
+@Data
 public class ProductCategory {
     @Id
     @GeneratedValue
+    //类目id
     private Integer categoryId;
+    //类目名称
     private String categoryName;
+    //类目编号
     private Integer categoryType;
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                '}';
-    }
 }
