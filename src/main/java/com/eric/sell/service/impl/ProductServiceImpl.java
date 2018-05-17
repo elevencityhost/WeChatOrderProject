869a162869a8b88b9fcfbf644ad63b8c44017fbb
elevencityhost <15901506742@163.com>
2018-05-18 00:07:39 +0800

    ProductService接口实现类编写

---
+++ src/main/java/com/eric/sell/service/impl/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.eric.sell.service.impl;
+
+import com.eric.sell.ProductStatusEnum;
+import com.eric.sell.dao.ProductInfoRepository;
+import com.eric.sell.model.ProductInfo;
+import com.eric.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * function
+ *
+ * @author 十一城城主
+ * @data 2018/5/17 23:27
+ */
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findById(productId).get();
+    }
+
+    @Override
+    public List<ProductInfo> fingUpAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+}
