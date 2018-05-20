c56d5355f412666b4a4e49bc4ce282fde8609870
elevencityhost <15901506742@163.com>
2018-05-20 10:04:35 +0800

    类目jpa中增加根据类目编号集合查询类目信息的方法，返回list

--- src/main/java/com/eric/sell/dao/ProductCategoryRepository.java
+++ src/main/java/com/eric/sell/dao/ProductCategoryRepository.java
@@ -3,11 +3,14 @@ package com.eric.sell.dao;
 import com.eric.sell.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * function
  *
  * @author 十一城城主
  * @data 2018/5/15 0:03
  */
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Integer> {
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 }
