2b4ffcaa0c7125c77cd650bd014eb1eb0bcda13d
elevencityhost <15901506742@163.com>
2018-05-21 09:44:44 +0800

    BuyerProductController查询所有商品信息包括类目

--- src/main/java/com/eric/sell/controller/BuyerProductController.java
+++ src/main/java/com/eric/sell/controller/BuyerProductController.java
@@ -1,14 +1,24 @@
 package com.eric.sell.controller;
 
+import com.eric.sell.model.ProductCategory;
+import com.eric.sell.model.ProductInfo;
+import com.eric.sell.service.ProductCategoryService;
+import com.eric.sell.service.ProductService;
+import com.eric.sell.util.ResultVOUtil;
 import com.eric.sell.vo.ProductInfoVO;
 import com.eric.sell.vo.ProductVO;
 import com.eric.sell.vo.ResultVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * function
@@ -19,23 +29,44 @@ import java.util.Arrays;
 @RestController
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductCategoryService productCategoryService;
     @GetMapping("/list")
     public ResultVO list() {
-        ProductInfoVO productInfoVO = new ProductInfoVO();
-        productInfoVO.setProductId("123");
-        productInfoVO.setProductName("酸辣粉");
-        productInfoVO.setProductPrice(new BigDecimal(5.3));
-        productInfoVO.setProductDescription("好吃不贵");
-        productInfoVO.setProductIcon("https://xxxxxxx.jpg");
-        ProductVO productVO = new ProductVO();
-        productVO.setCategoryName("热销");
-        productVO.setCategoryType(10);
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-        ResultVO<ProductVO> resultVO = new ResultVO<ProductVO>();
-        resultVO.setCode(0);
-        resultVO.setMsg("查询成功");
-        resultVO.setData(Arrays.asList(productVO));
-        return resultVO;
+        //查询所有上架商品
+        List<ProductInfo> productInfoList = productService.fingUpAll();
+        //查询所有商品的类目
+        //List<Integer> categoryTypeList = new ArrayList<>();
+        //传统方式
+        /*for (ProductInfo productInfo:productInfoList){
+            categoryTypeList.add(productInfo.getCategoryType());
+        }*/
+        //精简方式(java8 lambda)
+        List<Integer> categoryTypeList = productInfoList.stream()
+                .map(e -> e.getCategoryType())
+                .collect(Collectors.toList());
+        List<ProductCategory> categoryList = productCategoryService.findByCategoryTypeIn(categoryTypeList);
+        //数据拼装
+        List<ProductVO> productVOList = new ArrayList<ProductVO>();
+        for (ProductCategory productCategory:categoryList){
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryName(productCategory.getCategoryName());
+            productVO.setCategoryType(productCategory.getCategoryId());
+            List<ProductInfoVO> productInfoVOList = new ArrayList<ProductInfoVO>();
+            for (ProductInfo productInfo:productInfoList){
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())){
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo,productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        return ResultVOUtil.success(productVOList);
     }
 
 }
