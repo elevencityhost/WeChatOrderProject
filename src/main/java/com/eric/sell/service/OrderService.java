d733c8ad8b5bf0917d5006b40e9efec871c7b45a
elevencityhost <15901506742@163.com>
2018-05-22 23:32:13 +0800

    订单service(创建订单、查询订单列表、完结订单、取消订单、支付订单)

---
+++ src/main/java/com/eric/sell/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.eric.sell.service;
+
+import com.eric.sell.dto.OrderDTO;
+import com.eric.sell.model.OrderMaster;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * function
+ *
+ * @author 十一城城主
+ * @data 2018/5/22 22:12
+ */
+public interface OrderService {
+    /** 创建订单 */
+    OrderDTO create(OrderDTO orderDTO);
+    /** 查询单个订单 */
+    OrderDTO findOne(String orderId);
+    /** 查询订单列表 */
+    Page<OrderDTO> findList(String buyerOpenid, Pageable pageable);
+    /** 取消订单 */
+    OrderDTO cancel(OrderDTO orderDTO);
+    /** 完结订单 */
+    OrderDTO finish(OrderDTO orderDTO);
+    /** 支付订单 */
+    OrderDTO paid(OrderDTO orderDTO);
+}
