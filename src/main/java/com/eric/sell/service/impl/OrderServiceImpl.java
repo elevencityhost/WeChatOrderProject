47d1d9032d45d6b4909262aeb38e1bd4a1f38508
elevencityhost <15901506742@163.com>
2018-05-23 22:51:22 +0800

    添加订单

---
+++ src/main/java/com/eric/sell/service/impl/OrderServiceImpl.java
@@ -0,0 +1,101 @@
+package com.eric.sell.service.impl;
+
+import com.eric.sell.ResultEnum;
+import com.eric.sell.dao.OrderDetailRepository;
+import com.eric.sell.dao.OrderMasterRepository;
+import com.eric.sell.dto.OrderDTO;
+import com.eric.sell.exception.SellException;
+import com.eric.sell.model.CartDTO;
+import com.eric.sell.model.OrderDetail;
+import com.eric.sell.model.OrderMaster;
+import com.eric.sell.model.ProductInfo;
+import com.eric.sell.service.OrderService;
+import com.eric.sell.service.ProductService;
+import com.eric.sell.util.KeyUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * function
+ *
+ * @author 十一城城主
+ * @data 2018/5/22 22:30
+ */
+@Service
+public class OrderServiceImpl implements OrderService{
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
+
+    @Override
+    public OrderDTO create(OrderDTO orderDTO) {
+        String orderId = KeyUtil.genUniqueKey();
+        BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+        //查询商品（数量、价格）
+        for (OrderDetail orderDetail:orderDTO.getOrderDetailList()){
+            ProductInfo productInfo = productService.findOne(orderDetail.getProductId());
+            if (productInfo == null){
+                new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            //计算订单总价
+            orderAmount = orderDetail.getProductPrice()
+                    .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                    .add(orderAmount);
+            //订单详情入库
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo,orderDetail);
+            orderDetailRepository.save(orderDetail);
+        }
+        //写入订单数据库（OrderMaster、OrderDetail）
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId(orderId);
+        orderMaster.setOrderAmount(orderAmount);
+        BeanUtils.copyProperties(orderDTO,orderMaster);
+        orderMasterRepository.save(orderMaster);
+        //扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e ->
+        new CartDTO(e.getProductId(),e.getProductQuantity())
+        ).collect(Collectors.toList());
+        productService.decreaseStock(cartDTOList);
+
+        return null;
+    }
+
+    @Override
+    public OrderDTO findOne(String orderId) {
+        return null;
+    }
+
+    @Override
+    public Page<OrderDTO> findList(String buyerOpenid, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public OrderDTO cancel(OrderDTO orderDTO) {
+        return null;
+    }
+
+    @Override
+    public OrderDTO finish(OrderDTO orderDTO) {
+        return null;
+    }
+
+    @Override
+    public OrderDTO paid(OrderDTO orderDTO) {
+        return null;
+    }
+}
