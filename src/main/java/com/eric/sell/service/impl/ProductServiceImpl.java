9b029e512ed02e3cd21a2ff4baf5e82224c9e537
elevencityhost <15901506742@163.com>
2018-05-24 16:17:58 +0800

    商品serviceImpl

--- src/main/java/com/eric/sell/service/impl/ProductServiceImpl.java
+++ src/main/java/com/eric/sell/service/impl/ProductServiceImpl.java
@@ -1,13 +1,17 @@
 package com.eric.sell.service.impl;
 
 import com.eric.sell.ProductStatusEnum;
+import com.eric.sell.ResultEnum;
 import com.eric.sell.dao.ProductInfoRepository;
+import com.eric.sell.exception.SellException;
+import com.eric.sell.model.CartDTO;
 import com.eric.sell.model.ProductInfo;
 import com.eric.sell.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -41,5 +45,27 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public ProductInfo save(ProductInfo productInfo) {
         return repository.save(productInfo);
+    }
+
+    @Override
+    public void increaseStock(List<CartDTO> cartDTOList) {
+
+    }
+
+    @Override
+    @Transactional
+    public void decreaseStock(List<CartDTO> cartDTOList) {
+        for (CartDTO cartDTO:cartDTOList){
+            ProductInfo productInfo = repository.findById(cartDTO.getProductId()).get();
+            if (productInfo == null){
+                new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            Integer result = productInfo.getProductStock() - cartDTO.getProductQuantity();
+            if (result < 0){
+                new SellException(ResultEnum.PRODUCT_STOCK_ERROR);
+            }
+            productInfo.setProductStock(result);
+            repository.save(productInfo);
+        }
     }
 }
