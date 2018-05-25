0d80cc10415c26bb4a78341c1b1849e781daf4c4
elevencityhost <15901506742@163.com>
2018-05-25 18:43:23 +0800

    增加一些ResultEnum状态

--- src/main/java/com/eric/sell/ResultEnum.java
+++ src/main/java/com/eric/sell/ResultEnum.java
@@ -11,7 +11,13 @@ import lombok.Getter;
 @Getter
 public enum ResultEnum {
     PRODUCT_NOT_EXIST(10,"商品不存在"),
-    PRODUCT_STOCK_ERROR(11,"库存不足")
+    PRODUCT_STOCK_ERROR(11,"库存不足"),
+    ORDER_NOT_EXIST(12,"订单不存在"),
+    ORDERDETAIL_NOT_EXIST(13,"订单详情不存在"),
+    ORDER_STATUS_ERROR(14,"；订单状态不正确"),
+    ORDER_UPDATE_ERROR(15,"订单更新失败"),
+    ORDER_DETAIL_EMPTY(16,"订单中无商品详情"),
+    ORDER_PAY_STATUS_ERROR(17,"订单支付状态不正确")
     ;
     private Integer code;
 
