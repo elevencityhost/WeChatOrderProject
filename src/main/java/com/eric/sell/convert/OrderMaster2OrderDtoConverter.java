f4234e40ccf0c95a7e9997f0ed58d1e85b382de6
elevencityhost <15901506742@163.com>
2018-05-26 23:46:24 +0800

    订单转化器

---
+++ src/main/java/com/eric/sell/convert/OrderMaster2OrderDtoConverter.java
@@ -0,0 +1,25 @@
+package com.eric.sell.convert;
+
+import com.eric.sell.dto.OrderDTO;
+import com.eric.sell.model.OrderMaster;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * function
+ *
+ * @author 十一城城主
+ * @data 2018/5/25 15:18
+ */
+public class OrderMaster2OrderDtoConverter {
+    public static OrderDTO convert(OrderMaster orderMaster){
+        OrderDTO orderDTO = new OrderDTO();
+        BeanUtils.copyProperties(orderMaster,orderDTO);
+        return orderDTO;
+    }
+    public static List<OrderDTO> convert(List<OrderMaster> orderMasterList){
+        return orderMasterList.stream().map(e -> convert(e)).collect(Collectors.toList());
+    }
+}
